8c745234403583a4d883ad572079f48d55ecf82d
Oguri Cap <ahmadsaddam1112@gmail.com>
2025-08-11 11:38:35 +0700

    Initial commit: membuat education section menjadi responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,20 @@ module.exports = {
     "./*.html"
   ],
   theme: {
+    screens: {
+      'xs': '480px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
     extend: {
       fontFamily: {
         'primary': ['Poppins', 'sans-serif'],
+      },
+      inset: {
+        '17': '68px',
+        '26': '104px'
       }
     },
   },
